e0929ebe801e6a82e0a3a107e37180af48562bd3
Doughmination <clovetwilight3@outlook.com>
2026-02-25 12:25:35 +0000

    Update pc.ts

--- src/commands/utils/pc.ts
+++ src/commands/utils/pc.ts
@@ -3,16 +3,12 @@ import {
     SlashCommandSubcommandBuilder
 } from 'discord.js';
 
-const PC_URL = 'https://uk.pcpartpicker.com/list/pJy2FP';
-
 export default {
     data: new SlashCommandSubcommandBuilder()
-        .setName('pc')
-        .setDescription('Get my PC Part Picker URL')
-        ),
+        .setName('invite')
+        .setDescription('Display personal server invite link'),
 
     async execute(interaction: ChatInputCommandInteraction) {
-            await interaction.reply(PC_URL);
-        }
+        await interaction.reply(`https://uk.pcpartpicker.com/list/pJy2FP`);
     }
 };
